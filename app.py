d3ecaa6928a3a7bc3bd92da5ecd859f42d0f6de3
LapuGitHub <47387367+LapuGitHub@users.noreply.github.com>
2019-04-14 15:41:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-import os
-from bottle import route, run
-
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
-
-
-if __name__ == '__main__':
-    port = os.environ.get('PORT', 8000)
-
-    run(host='127.0.0.1', port=port)
+#!/usr/bin/env python
+from http.server import BaseHTTPRequestHandler, HTTPServer
+# Creiamo la classe che riceverà e risponderà alla richieste HTTP
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+  # Implementiamo il metodo che risponde alle richieste GET
+  def do_GET(self):
+        # Specifichiamo il codice di risposta
+        self.send_response(200)
+        # Specifichiamo uno o più header
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Specifichiamo il messaggio che costituirà il corpo della risposta
+        message = "Hello world!"
+        self.wfile.write(bytes(message, "utf8"))
+        return
+def run():
+  print('Avvio del server...')
+  # Specifichiamo le impostazioni del server
+  # Scegliamo la porta 8081 (per la porta 80 sono necessari i permessi di root)
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('Server in esecuzione...')
+  httpd.serve_forever()
+run()
